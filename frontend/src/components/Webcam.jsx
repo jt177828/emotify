9525ec15c900b112aaf28367aca84eb6fc746a7c
justin-tian <justin.tian.372@gmail.com>
2025-01-25 18:12:54 -0500

    remove axios

--- frontend/src/components/Webcam.jsx
+++ frontend/src/components/Webcam.jsx
@@ -1,6 +1,5 @@
 import React, { useRef, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 
 const Webcam = () => {
   const videoRef = useRef(null);
